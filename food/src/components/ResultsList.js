d8a8f9a0aeb5e32cd642328ca47bfb046d340248
izharj <eezhar@gmail.com>
2023-03-21 19:54:44 +0800

    updated additional screen to show results for specific pricing category.

--- food/src/components/ResultsList.js
+++ food/src/components/ResultsList.js
@@ -12,6 +12,11 @@ import { useNavigation } from "@react-navigation/native";
 
 const ResultsList = ({ title, results }) => {
   const navigation = useNavigation();
+
+  if (!results.length) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
@@ -23,7 +28,13 @@ const ResultsList = ({ title, results }) => {
         keyExtractor={(result) => result.id}
         renderItem={({ item }) => {
           return (
-            <TouchableOpacity onPress={() => navigation.navigate("Results")}>
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate("Results", {
+                  id: item.id,
+                })
+              }
+            >
               <ResultsDetail result={item} />
             </TouchableOpacity>
           );
--- food/src/screens/ResultsShowScreen.js
+++ food/src/screens/ResultsShowScreen.js
@@ -1,14 +1,46 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet, FlatList, Image } from "react-native";
+import React, { useEffect, useState } from "react";
+import yelp from "../api/yelp";
 
-const ResultsShowScreen = () => {
+const ResultsShowScreen = ({ route }) => {
+  const [result, setResult] = useState(null);
+  const { id } = route.params;
+
+  //   console.log(id);
+  //   console.log(result);
+
+  const getResult = async (id) => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+
+  useEffect(() => {
+    getResult(id);
+  }, []);
+
+  if (!result) {
+    return null;
+  }
+
   return (
     <View>
-      <Text>ResultsShowScreen</Text>
+      <Text>{result.name}</Text>
+      <FlatList
+        keyExtractor={(photo) => photo}
+        data={result.photos}
+        renderItem={({ item }) => {
+          return <Image style={styles.image} source={{ uri: item }} />;
+        }}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    height: 200,
+    width: 300,
+  },
+});
 
 export default ResultsShowScreen;
